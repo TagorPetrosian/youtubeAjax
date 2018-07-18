3154f51442ad79b622bd939d6289b0298416f36d
Tagor Petrosian <p.tagor@gmail.com>
2018-07-18 13:19:40 +0300

    implemented video-detail component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search-bar";
 import VideoList from "./components/video-list";
+import VideoDetail from "./components/video-detail";
 import keys from "../config/keys";
 
 const API_KEY = keys.YOUTUBE_API_KEY;
@@ -13,7 +14,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+    YTSearch({ key: API_KEY, term: "jazz" }, videos => {
       this.setState({ videos });
     });
   }
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
