9d3da9988814b2606ab862d541b57ea07bc6a6e7
Tagor Petrosian <p.tagor@gmail.com>
2018-07-18 14:41:36 +0300

    added search functionality

--- src/index.js
+++ src/index.js
@@ -17,17 +17,22 @@ class App extends Component {
       seletectedVideo: null
     };
 
-    YTSearch({ key: API_KEY, term: "game of thrones" }, videos => {
+    this.videoSearch("game of thrones");
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
